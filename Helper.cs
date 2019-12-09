8e7e823e7899bea7470df0ec5d5ecfa9c7c64851
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-09 19:21:37 +0300

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Romanian
+{
+    public static class  Helper
+    {
+        public static string ToRomanian(this int input)
+        {
+            int[] mas1 = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] mas2 = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int i = 0;         
+            string s = "";
+            while (input > 0) 
+            {
+                if (mas1[i] <= input)
+                {                     
+                    input -= mas1[i];   
+                    s += mas2[i];
+                }                      
+                else 
+                    i++;
+            }
+            return s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Romanian
+
+{
+    /*
+     * extension method, конвертирующий int в строку с римским представлением этого числа: 24.ToRomanaian() => "XXIV"
+     */
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число = ");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            string Romanian = numbers.ToRomanian();
+            Console.WriteLine($"Число в римской системе = {Romanian}");
+            Console.ReadLine();
+        }
+    }
+}
